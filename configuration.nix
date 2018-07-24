1523beed120c1c68c52f78358bc2b17b086a361f
Michael Fitzmayer <mail@michael-fitzmayer.de>
2018-07-24 11:24:56 +0200

    Add clamav

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,9 @@
   programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   # List services that you want to enable:
+  services.clamav.daemon.enable        = true;
+  services.clamav.updater.config       = true;
+  services.clamav.updater.enable       = true;
   services.cron.enable                 = true;
   services.logrotate.enable            = true;
   services.rsyslogd.enable             = true;
